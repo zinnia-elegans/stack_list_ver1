cc77ef5ee1c11eb8ceb058eaa0c6fda66bd8c8f0
zinnia-elegans <sono.genoano@gmail.com>
2020-05-17 16:14:08 +0900

    初回コミット

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
                                     <div class="media-body">
                                         <div class="media m-3 d-block text-center">
                                             <h2 class="media-heading"><strong>ログイン方法</strong></h2>
-                                            <h3 class="m-5"><strong>当サイトの登録には、OAuthという認証方法を利用しております。サービス利用時、あなたのTwitterパスワードを知らせることはありません。</strong></h3>
+                                            <h3 class="m-5"><strong>当サイトの登録には、OAuthという認証方法を利用しております。</strong></h3>
                                             <h3 class="m-5">OAuth認証では、ユーザー情報にアクセスする情報を得るだけで、当サイトがサービスのアカウントからユーザー情報を得ることができます。</h3>
                                             <a href="{{ url('auth/twitter') }}">
                                                 <button type="button" class="btn btn-primary"><i class="fab fa-twitter"></i> Twitterアカウントでログインする</button>
