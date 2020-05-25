6c1c77ebffe79f5896026a9df51a8ee7b29e3409
zinnia-elegans <sono.genoano@gmail.com>
2020-05-25 21:34:14 +0900

    ルート調整

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,24 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-// 認証ページ
-Route::get('/auth/{provider}', 'Auth\LoginController@redirectToProvider');
-Route::get('/auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
 // トップページ
 Route::get('/','TweetsController@home')->name('home');
 Route::get('/about', 'TweetsController@about');
 
+// 認証ページ
+Route::get('/auth/{provider}', 'Auth\LoginController@redirectToProvider');
+Route::get('/auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+
+// 認証後ページ
 Route::group(['middleware' => 'auth'], function () {
+    // ツイッターユーザー用
     Route::get('admin', 'TweetsController@admin')->name('admin');
     Route::post('admin', 'TweetsController@admin');
-    Route::get('guest/register', 'UserController@getProfile')->name('register');
+    // ゲストユーザー用
+    Route::get('guest/register', 'UserController@getProfile');
     Route::get('guest/guestuser', 'UserController@admin');
+
+
     Route::get('continue', 'ContinueController@continue');
     Route::post('continue', 'ContinueController@continue');
 });
