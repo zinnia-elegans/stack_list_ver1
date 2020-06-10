dc3f8f44c9443622cc197046a36262823e8ded4c
zinnia-elegans <sono.genoano@gmail.com>
2020-06-10 15:48:12 +0900

    fix:余計なviewを削除

--- resources/views/users/admin.blade.php
+++ resources/views/users/admin.blade.php
@@ -9,17 +9,7 @@
         <h2 class="m-4">積み上げ日数</h2>
           <form action="{{ url('/') }}" method="post">
             <div class="card-body border m-3">
-              <input type="radio" class="m-3" name="continue" value="できてる">開始日から計算して継続日数を登録
-                @csrf
-                <div class="form-group m-3">
-                    <input id="year" type="text" name="y" size="6" maxlength="4" value="2020"/> 年 
-                    <input id="month" type="text" name="m" size="4" maxlength="2" value="{{ old('m') }}" placeholder="yy"/> 月 
-                    <input id="date" type="text" name="d" size="4" maxlength="2" placeholder="dd"/> 日
-                </div>
-              </form>
-            </div>
-            <div class="card-body border m-3">
-              <input type="radio" class="m-3">手動で積み上げ日数を登録<br>
+              <input type="radio" class="m-3">登録した日数<br>
               <input type="text" size="8" maxlength="2" class="m-3"> 日
             </div>
               @foreach($stacklistday as $beforeList)
