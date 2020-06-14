2ae5dc26e2ff0ffa1673a920ee52170d3281c03c
zinnia-elegans <sono.genoano@gmail.com>
2020-06-14 21:29:49 +0900

    fix:cssバグ修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="ja">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
